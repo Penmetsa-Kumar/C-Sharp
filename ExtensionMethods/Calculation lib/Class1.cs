80dd4f6de415fd15e7df5ff0cccc562c10fe136d
kumar raju <kumar10raju2@gmail.com>
2020-12-17 20:09:45 +0530

    Add files via upload

---
+++ ExtensionMethods/Calculation lib/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public static class Calculator 
+    {
+        public static float Addition(float a,float b)
+        {
+            return a + b;
+        }
+        public static float Substract(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }   
+
+        public static float Power(float a)
+        {
+            return a * a;
+        }
+    }
+}
